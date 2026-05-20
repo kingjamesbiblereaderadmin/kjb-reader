00319a3d997f852430b76ef8a514a2f2c5c748f2
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-05-20 14:00:59 +0000

    File changes

--- src/pages/BibleReader.jsx
+++ src/pages/BibleReader.jsx
@@ -607,7 +607,6 @@ export default function BibleReader() {
                     )}
                   </div>
                 </div>
-              </div>
               )}
               {/* Mobile bottom sheet */}
               <SelectorSheet open={showZoomPopover && isMobile()} onClose={() => setShowZoomPopover(false)} title="Text Size">
