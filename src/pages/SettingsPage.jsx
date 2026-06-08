6a442ad090c142af87a2a016ed1b60c97f927329
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-06-08 11:41:46 +0000

    File changes

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -34,7 +34,7 @@ const inIframe = () => {
 };
 
 const LAST_REVISED = 'June 8th, 2026';
-const WORKER_VERSION = 'v20260608_202';
+const WORKER_VERSION = 'v20260608_203';
 
 export default function SettingsPage() {
   const navigate = useNavigate();
