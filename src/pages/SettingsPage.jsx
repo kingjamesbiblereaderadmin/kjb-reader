5d20f3c9d0a6c621d896edd03050921bd98390e8
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-06-08 07:21:18 +0000

    File changes

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -30,7 +30,7 @@ const A11Y_FONTS = [
 ];
 
 const LAST_REVISED = 'June 8th, 2026';
-const WORKER_VERSION = 'v20260608_152';
+const WORKER_VERSION = 'v20260608_153';
 
 export default function SettingsPage() {
   const navigate = useNavigate();
