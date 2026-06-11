a3912a8bc9cc4fbad52c52769b09618f98c594e2
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-06-11 20:06:52 +0000

    File changes

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
-// KJB Reader Service Worker v20260611_358
+// KJB Reader Service Worker v20260611_359
 // Cache-first loading for offline support
 
-const CACHE_NAME = 'kjb-reader-v20260611_358';
+const CACHE_NAME = 'kjb-reader-v20260611_359';
 // Bumped to force complete reinstall for all users
 const LEGACY_CACHE_NAME = 'kjb-legacy-v9';
 
