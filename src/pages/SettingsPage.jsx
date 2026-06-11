360a471168fc3db15aa36ac57bae86dcf0408a33
Shawn Poh Hanlin <shawnlovesdeerrhorsesavi@gmail.com>
2026-06-11 11:00:24 +0000

    File changes

--- src/pages/SettingsPage.jsx
+++ src/pages/SettingsPage.jsx
@@ -51,7 +51,7 @@ const isBookmarkBrowser = () => {
 };
 
 const LAST_REVISED = 'June 11th, 2026';
-const WORKER_VERSION = 'v20260611_322';
+const WORKER_VERSION = 'v20260611_323';
 
 export default function SettingsPage() {
   const navigate = useNavigate();
